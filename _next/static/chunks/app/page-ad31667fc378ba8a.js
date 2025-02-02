8f78770b442e7379de2ec99d9f0cfcf3dc766b2e
dev0xstar <angrybird4837@gmail.com>
2025-02-02 02:37:40 -0500

    updated

---
+++ _next/static/chunks/app/page-ad31667fc378ba8a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{2329:function(e,n,s){Promise.resolve().then(s.bind(s,1465)),Promise.resolve().then(s.bind(s,2852))}},function(e){e.O(0,[975,716,550,744],function(){return e(e.s=2329)}),_N_E=e.O()}]);
